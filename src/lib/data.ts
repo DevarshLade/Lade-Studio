cd01ccd8c622946f04355630addecb5694c1a8e4
Devarsh Lade <ladedevarsh@gmail.com>
2025-08-19 16:57:06 +0000

    Make the following changes:
    ,
    
    Find this element:
    
    Relevant files:

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -205,7 +205,7 @@ export const blogPosts: BlogPost[] = [
   {
     id: 'blog-1',
     title: 'The Story Behind My Latest Collection',
-    image: 'https://placehold.co/400x250.png',
+    image: 'https://images.unsplash.com/photo-1678271248126-160b859ea228?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHwxN3x8VGhlJTIwU3RvcnklMjBCZWhpbmQlMjBNeSUyMExhdGVzdCUyMENvbGxlY3Rpb258ZW58MHx8fHwxNzU1NjIyMjkyfDA&ixlib=rb-4.1.0&q=80&w=1080',
     excerpt: 'Dive deep into the inspiration and process that brought the "Forest Whispers" collection to life. From initial sketches to the final brushstroke.',
     hint: 'art studio'
   },
