f09e1f9c0ef217f3c53e841b1319ec316099670d
Devarsh Lade <ladedevarsh@gmail.com>
2025-08-19 18:02:27 +0000

    in 'contact' page show only jotform's 'Contact Us' embed code script for

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,6 +1,12 @@
 
+"use client";
+
+import Script from 'next/script';
+
 export default function ContactPage() {
   return (
-    <div></div>
+    <div className="container mx-auto px-4 py-12">
+      <Script type="text/javascript" src="https://form.jotform.com/jsform/252305862627459" id="jotform-script" />
+    </div>
   );
 }
